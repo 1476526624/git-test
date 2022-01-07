3ec6a275a572c4a4eff2eb4a437e0f0833eb1924
lazar <1476526624@qq.com>
2022-01-07 11:38:39 +0800

    master commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,9 +2,11 @@ package com.atguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
+        //commit之前已经add过的文件时，可以不用再次add，直接commit
         System.out.println("hello git");
         System.out.println("hello GitHub");
         System.out.println("hello World");
         System.out.println("If not me,who?");
+        System.out.println("If opportunities are presented to you");
     }
 }
